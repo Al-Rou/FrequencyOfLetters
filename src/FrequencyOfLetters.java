1b0be4866fe003ab98de788ce906790ad9d562bf
Al-Rou <roal.24.alro@gmail.com>
2021-07-04 01:39:02 -0400

    Commented

--- src/FrequencyOfLetters.java
+++ src/FrequencyOfLetters.java
@@ -5,14 +5,17 @@ public class FrequencyOfLetters {
     {
         List<Character> list = new ArrayList<>();
         Map<Character, Integer> map = new HashMap<>();
-
+        //Prompt the user to enter their sentence
         System.out.println("Enter your sentence: ");
         Scanner in = new Scanner(System.in);
+        //Take the entered sentence
         String input = in.nextLine();
+        //Separate all letters and store them one by one in a list
         for(int i = 0; i < input.length(); i++)
         {
             list.add(i, input.charAt(i));
         }
+        //Count letters' frequency
         while (list.size() != 0)
         {
             char aux = list.get(0);
@@ -20,6 +23,7 @@ public class FrequencyOfLetters {
             list.remove(0);
             for(int i = 0; i < list.size(); i++)
             {
+                //No matter if small or capital letters
                 if((list.get(i) == aux) || (list.get(i) == aux-32) || (list.get(i) == aux+32))
                 {
                     counter++;
@@ -27,15 +31,19 @@ public class FrequencyOfLetters {
                     i--;
                 }
             }
+            //Store any letter with its occurrence
             map.put(aux, counter);
         }
+        //Print results out
         for(Character a : map.keySet())
         {
+            //If it is space, print "space" out instead " "!
             if(a == ' ')
             {
                 System.out.print("space -> " + map.get(a) + "\n");
             }
             else {
+                //Print out just letters in capital form
                 if((int) a > 96)
                 {
                     System.out.print((char) (a-32) + " -> " + map.get(a) + "\n");
@@ -45,17 +53,23 @@ public class FrequencyOfLetters {
                 }
             }
         }
+        //Find the letter with maximum occurrence
         List<Integer> result = new ArrayList<>();
         int maxFre = 0;
+        //Take all values from the map, which are in fact the keys' occurrences
         for(Character a : map.keySet())
         {
             result.add(map.get(a));
         }
+        //Sort the occurrences
         Collections.sort(result);
+        //Take the maximum
         maxFre = result.get(result.size()-1);
         for(Character a : map.keySet())
         {
+            //Show the letters with the maximum occurrence
             if(map.get(a) == maxFre) {
+                //If space, go for the second maximal occurrence
                 if(a == ' ')
                 {
                     maxFre = result.get(result.size()-2);
